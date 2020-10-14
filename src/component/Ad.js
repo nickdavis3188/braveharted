2e805c982c92036fed3eecd86ab00361b1997796
nickdavis3188 <72302925+nickdavis3188@users.noreply.github.com>
2020-10-14 15:32:11 +0100

    Create Ad.js

---
+++ src/component/Ad.js
@@ -0,0 +1,21 @@
+import React, {Component } from 'react';
+class Ad extends Component {
+    componentDidMount(){
+        (adsbygoogle = window.adsbygoogle || []).push({});
+    }
+    render() { 
+        return(
+            <div className="ad">
+                <ins className="adsbygoogle"
+                    style={{display:"block"}}
+                    data-ad-client="ca-pub-5641450851836516"
+                    data-ad-slot="4758775619"
+                    data-ad-format="auto"
+                    data-full-width-responsive="true">
+                </ins>
+            </div>
+        ) 
+    }
+}
+ 
+export default Ad;
